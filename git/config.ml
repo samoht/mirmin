a3fa29f3b23e7a95b72c4e1156d8e5f0ecc58808
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-03-04 21:45:53 +0000

    Allow to compile nicely under unix and osx

--- git/config.ml
+++ git/config.ml
@@ -5,6 +5,18 @@ let stack = direct_stackv4_with_default_ipv4 default_console tap0
 let main = foreign "Unikernel.Main" @@ console @-> stackv4 @-> job
 
 let () =
-  add_to_ocamlfind_libraries [ "zlib"; "conduit.lwt"; "conduit.mirage"; "dns.mirage"; "git.mirage" ];
+  add_to_ocamlfind_libraries [
+    "conduit.lwt"; "conduit.mirage"; "dns.mirage"; "git.mirage"
+  ];
+  match Mirage.get_mode () with
+  | `Xen -> add_to_ocamlfind_libraries ["zlib.xen"]
+  | _    -> ()
+
+let () =
   add_to_opam_packages [ "mirage-dns"; "conduit"; "git" ];
+  match Mirage.get_mode () with
+  | `Xen -> add_to_opam_packages ["zlib-xen"]
+  | _    -> ()
+
+let () =
   register "git" [ main $ default_console $ stack ]
