9a0bda3181d1fb34c08f80a68d2ebe2483ed5d72
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-07-13 16:05:17 +0100

    Clean-up the Git unikernel
    Some of the bits should be pushed upstream to mirage-conduit

--- git/unikernel.ml
+++ git/unikernel.ml
@@ -4,30 +4,31 @@ let () =
   Log.set_time (fun () -> OS.Time.Monotonic.(to_seconds (time ())))
 *)
 
-let (>>=) = Lwt.bind
+open Lwt.Infix
+
 let uri_str = "https://github.com/samoht/opam-repository.git"
 let gri = Git.Gri.of_string uri_str
 
 module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
 
-  module DNS = Dns_resolver_mirage.Make(OS.Time)(S)
-  module RES = Resolver_mirage.Make(DNS)
-
-  module Mirage_sync = Git_mirage.Sync
-  module Sync = Mirage_sync.Make(Git.Memory)
-
   let log_s c fmt = Printf.ksprintf (C.log_s c) fmt
 
-  let conduit = Conduit_mirage.empty
-  let stackv4 = Conduit_mirage.stackv4 (module S)
+  module Sync = Git_mirage.Sync.Make(Git.Memory)
 
-  let start c stack =
-    log_s c "Starting ..." >>= fun () ->
+  let ctx stack =
+    let module DNS = Dns_resolver_mirage.Make(OS.Time)(S) in
+    let module RES = Resolver_mirage.Make(DNS) in
+    let conduit = Conduit_mirage.empty in
+    let stackv4 = Conduit_mirage.stackv4 (module S) in
     let res = Resolver_lwt.init () in
     RES.register ~stack res;
     Conduit_mirage.with_tcp conduit stackv4 stack >>= fun conduit ->
-    Conduit_mirage.with_tls conduit               >>= fun conduit ->
-    let ctx = res, conduit in
+    Conduit_mirage.with_tls conduit               >|= fun conduit ->
+    res, conduit
+
+  let start c stack =
+    log_s c "Starting ..." >>= fun () ->
+    ctx stack >>= fun ctx ->
 
     log_s c "Running 'git ls-remote %s'" uri_str >>= fun () ->
     Git.Memory.create () >>= fun t ->
